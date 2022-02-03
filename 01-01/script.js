36515bbc01907c58eb04067d0d75a9008b501e78
Tommy Palmer <hi@tommyp.org>
2022-02-03 15:58:22 +0000

    update homework

--- 01-01/script.js
+++ 01-01/script.js
@@ -1,26 +1,35 @@
-let font;
-let graphic;
+let one;
+let two;
 
 function preload() {
-  font = loadFont("./assets/spacegrotesk-medium.otf");
+  font = loadFont('./assets/spacegrotesk-medium.otf');
 }
 
 function setup() {
   createCanvas(500, 500);
-  graphic = createGraphics(500, 500);
+  one = createGraphics(500, 500);
 
-  graphic.textSize(400);
-  graphic.textFont(font);
-  graphic.fill("#ef5236");
-  graphic.textAlign(CENTER, CENTER);
-  graphic.text("01", width / 2, height / 2);
+  one.textSize(400);
+  one.textFont(font);
+  one.fill('#ef5236');
+  one.textAlign(CENTER, CENTER);
+  one.text('01', width / 2, height / 2);
+
+  two = createGraphics(500, 500);
+
+  two.textSize(400);
+  two.textFont(font);
+  two.fill('#ef5236');
+  two.textAlign(CENTER, CENTER);
+  two.text('23', width / 2, height / 2);
 }
 
 function draw() {
-  background("#ecf072");
+  background('#ecf072');
 
-  let val = sin(frameCount * 0.01) * 250;
+  let val = sin(frameCount * 0.05) * 250;
 
-  copy(graphic, 0, 0, 500, 500, 0, 0, 250 + val, 500);
-  copy(graphic, 0, 0, 500, 500, 250 + val, 0, 250 - val, 500);
+  copy(one, 0, 0, 250 + val, 500, 0, 0, 250 + val, 500);
+
+  copy(two, 250 + val, 0, 250 - val, 500, 250 + val, 0, 250 - val, 500);
 }
