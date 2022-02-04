c00422f04ce737d9ac96374c9b1c7d662c3a2cdf
Tommy Palmer <hi@tommyp.org>
2022-02-04 10:31:01 +0000

    fuck the tories

--- good-morning/script.js
+++ good-morning/script.js
@@ -1,4 +1,8 @@
 let font;
+let graphic;
+
+const color = '#2fe4c2';
+const bgColor = '#293752';
 
 function preload() {
   font = loadFont('./assets/spacegrotesk-medium.otf');
@@ -7,12 +11,40 @@ function preload() {
 function setup() {
   createCanvas(1200, 600);
 
-  fill('#ff0000');
-  textFont(font);
-  textSize(300);
-  textLeading(200);
-  textAlign(CENTER, CENTER);
-  text('Good\nMorning', 600, 300);
+  graphic = createGraphics(width, height);
+
+  graphic.fill(color);
+  graphic.textFont(font);
+  graphic.textSize(300);
+  graphic.textLeading(300);
+  graphic.textAlign(CENTER, CENTER);
+  graphic.text('fuck the\ntories', 600, 300);
 }
 
-function draw() {}
+function draw() {
+  background(bgColor);
+
+  const tileSize = 10;
+
+  for (let x = 0; x < width / tileSize; x++) {
+    for (let y = 0; y < height / tileSize; y++) {
+      let distortion = sin(frameCount * 0.05 + x * 0.3) * 50;
+
+      if (x % 2 == 0) {
+        distortion = cos(frameCount * 0.05 + x * 0.3) * -50;
+      }
+
+      const sx = x * tileSize;
+      const sy = y * tileSize;
+      const sw = Math.max(tileSize + distortion, tileSize);
+      const sh = Math.max(tileSize + distortion, tileSize);
+
+      const dx = x * tileSize;
+      const dy = y * tileSize;
+      const dw = Math.max(tileSize + distortion, tileSize);
+      const dh = tileSize;
+
+      image(graphic, dx, dy, dw, dh, sx, sy, sw, sh);
+    }
+  }
+}
