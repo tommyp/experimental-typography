4f73307d08c2281252334bffc1d73e31c5297622
Tommy Palmer <hi@tommyp.org>
2022-02-07 15:44:50 +0000

    add sliders

--- good-morning/script.js
+++ good-morning/script.js
@@ -2,6 +2,16 @@ let font;
 let graphic;
 
 const waveInput = document.querySelector('input.wave');
+const waveXInput = document.querySelector('input.waveX');
+const waveYInput = document.querySelector('input.waveY');
+const line1Input = document.querySelector('input.line1');
+const line2Input = document.querySelector('input.line2');
+
+[line1Input, line2Input].forEach((input) => {
+  input.addEventListener('input', () => {
+    createCopy();
+  });
+});
 
 const color = '#2fe4c2';
 const bgColor = '#293752';
@@ -13,14 +23,7 @@ function preload() {
 function setup() {
   createCanvas(1200, 600);
 
-  graphic = createGraphics(width, height);
-
-  graphic.fill(color);
-  graphic.textFont(font);
-  graphic.textSize(300);
-  graphic.textLeading(300);
-  graphic.textAlign(CENTER, CENTER);
-  graphic.text('Good\nMorning', 600, 250);
+  createCopy();
 }
 
 function draw() {
@@ -31,8 +34,10 @@ function draw() {
   for (let x = 0; x < 120; x++) {
     for (let y = 0; y < 60; y++) {
       const wave = waveInput.value;
-      const distortionX = sin(frameCount * wave + x * 0.5 + y * 0.3) * 10;
-      const distortionY = sin(frameCount * wave + x * 0.5 + y * 0.5) * 5;
+      const distortionX =
+        sin(frameCount * wave + x * 0.5 + y * 0.3) * waveXInput.value;
+      const distortionY =
+        sin(frameCount * wave + x * 0.5 + y * 1) * waveYInput.value;
 
       const sx = x * tileSize + distortionX;
       const sy = y * tileSize + distortionY;
@@ -47,4 +52,17 @@ function draw() {
       image(graphic, dx, dy, dw, dh, sx, sy, sw, sh);
     }
   }
+}
+
+function createCopy() {
+  graphic = createGraphics(width, height);
+
+  const text = line1Input.value + '\n' + line2Input.value;
+
+  graphic.fill(color);
+  graphic.textFont(font);
+  graphic.textSize(300);
+  graphic.textLeading(300);
+  graphic.textAlign(CENTER, CENTER);
+  graphic.text(text, 600, 250);
 }
