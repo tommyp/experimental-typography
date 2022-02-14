ac52c495d19362254e6a24fd3d10cf1ee06a6b39
Tommy Palmer <hi@tommyp.org>
2022-02-14 12:09:12 +0000

    draw text and line

--- dani-lewis/script.js
+++ dani-lewis/script.js
@@ -1,4 +1,5 @@
 let font;
+let points;
 
 function preload() {
   font = loadFont('./assets/inconsolata-bold.ttf');
@@ -6,11 +7,29 @@ function preload() {
 
 function setup() {
   createCanvas(1200, 600);
+
+  points = font.textToPoints('Dani Lewis', 130, 330, 180, {
+    sampleFactor: 0.1,
+    simplifyThreshold: 0,
+  });
 }
 
 function draw() {
   background('#e84e3c');
-  textFont(font);
-  textSize(32);
-  text('Dani Lewis', 100, 100);
+
+  noStroke();
+  fill('#f3c043');
+
+  points.forEach((point) => {
+    circle(point.x, point.y, 5);
+  });
+
+  noFill();
+  stroke('#f3c043');
+
+  beginShape();
+  points.forEach((point) => {
+    vertex(point.x, point.y);
+  });
+  endShape();
 }
