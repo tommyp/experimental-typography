fe033c59eeadc02f2076d9f22eb878a68e74195a
Tommy Palmer <hi@tommyp.org>
2022-02-14 12:26:26 +0000

    add wavy line

--- dani-lewis/script.js
+++ dani-lewis/script.js
@@ -15,6 +15,7 @@ function setup() {
 }
 
 function draw() {
+  const nl = 0.01;
   background('#e84e3c');
 
   noStroke();
@@ -35,7 +36,10 @@ function draw() {
     const distance = createVector(point.x - mouseX, point.y - mouseY);
     const distortion = distance.mult(40 / distance.mag());
 
-    vertex(point.x + distortion.x, point.y + distortion.y);
+    const nx = 40 * noise(nl * point.x, nl * point.y, nl * frameCount) - 20;
+    const ny = 40 * noise(nl * point.x, nl * point.y, nl * frameCount) - 20;
+
+    vertex(point.x + distortion.x + nx, point.y + distortion.y + ny);
   });
   endShape();
 }
